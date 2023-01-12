9b3c3c007905c5cf076169cf030ee56e35a87089
Aleksander <silvershtain@protonmail.com>
2023-01-12 19:24:42 +0700

    Initial Commit

---
+++ CartoonDataApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CartoonDataApp
+//
+//  Created by Aleksandr on 12.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
