a8beebf3a53350380ee2e805cb3af770d56fcf65
Aleksander <silvershtain@protonmail.com>
2023-01-21 23:04:37 +0700

    setup CharacterView

--- CartoonDataApp/Controllers/Core/CDCharacterViewController.swift
+++ CartoonDataApp/Controllers/Core/CDCharacterViewController.swift
@@ -10,12 +10,22 @@ import UIKit
 /// Controller to show and search for Characters
 final class CDCharacterViewController: UIViewController {
 
+    private let characterListView = CharacterListView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground // адаптировано под темную и светлую тему
         title = R.Name.characters
         
+        view.addSubview(characterListView)
+        
+        NSLayoutConstraint.activate([
+            characterListView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            characterListView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            characterListView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            characterListView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
     }
 
 }
--- CartoonDataApp/Views/CharacterListView.swift
+++ CartoonDataApp/Views/CharacterListView.swift
@@ -9,14 +9,48 @@ import UIKit
 
 /// View that handles showing list of characters, loader, etc.
 final class CharacterListView: UIView {
+    
+    private let viewModel = CharacterListViewViewModel()
+    
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.hidesWhenStopped = true
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        return spinner
+    }()
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+    //MARK: - Init
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         translatesAutoresizingMaskIntoConstraints = false
-        backgroundColor = .systemBlue
+        addSubview(spinner)
+        
+        addConstraints()
+        
+        spinner.startAnimating()
+        viewModel.fetchCharacters()
     }
     
     required init?(coder: NSCoder) {
         fatalError("Unsupported")
+    }
+    
+    private func addConstraints() {
+        
+        NSLayoutConstraint.activate([
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor),
+            spinner.widthAnchor.constraint(equalToConstant: 100),
+            spinner.heightAnchor.constraint(equalToConstant: 100),
+        
+        ])
     }
 }
